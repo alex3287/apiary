1a1228bc681b52f046fb6f0bb51dbbb91027f4ce
alex3287 <alex3287@bk.ru>
2020-03-22 18:11:28 +0700

    create modal window

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let button = document.querySelector('#button');
+let modal = document.querySelector('#modal');
+let colose = document.querySelector('#close');
+
+button.addEventListener('click', function(){
+    modal.classList.add('modal_active');
+});
+
+modal.addEventListener('click', function(){
+    modal.classList.remove('modal_active');
+});
+
